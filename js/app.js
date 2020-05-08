49550561e7cf4fb4e97f1cb772ad4a49b4157035
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-05-08 14:17:19 -0500

    connects with TMDB api to download tranding movies

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const apiKey = '0b941991fb739be72fed42ae5e2a4891'
+
+fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
+  .then(resp => resp.json())
+  .then(json => console.log(json));
