237874fea89335276acd25d827f032083820c951
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-05-08 14:48:47 -0500

    Groups movies and genres together, into a comprehensive object

--- js/app.js
+++ js/app.js
@@ -6,8 +6,29 @@ fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
       .then(resp => resp.json())
       .then(genresResults => {
-        console.log(genresResults.genres);
-        console.log(moviesResults.results);
+        buildMovieGenreObject(moviesResults.results, genresResults.genres);
       });
+  });  
+
+// {"Adventure": [movie1]}
+function buildMovieGenreObject(movies, genres) {
+  movieGenres = {}
+
+  movies.forEach(movie => {
+    movie.genre_ids.forEach(genre_id => {
+      let movieGenre = genres.find(genre => genre.id === genre_id);
+      
+      if(movieGenres[movieGenre.name] === undefined) {
+        movieGenres[movieGenre.name] = [movie];
+      } else {
+        movieGenres[movieGenre.name].push(movie);
+      }
+    });
   });
-  
+
+  console.log(movieGenres);
+}
+
+function insertIntoDOM(crazyeMovieGenreObject) {
+
+}
