424a5467dcc9515aecbeb94ed811308303eb409e
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-05-08 14:30:15 -0500

    waits until retrieval of movies has completed before genres gets retrieved

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ const apiKey = '0b941991fb739be72fed42ae5e2a4891'
 
 fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
   .then(resp => resp.json())
-  .then(json => console.log(json));
-
-fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
-  .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(moviesResults => {
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
+      .then(resp => resp.json())
+      .then(genresResults => {
+        console.log(genresResults.genres);
+        console.log(moviesResults.results);
+      });
+  });
+  
