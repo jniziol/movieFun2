e961f8830a7f04a7936ca5f139dd514aa38e7ca8
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-05-08 15:11:04 -0500

    movies are inserted into DOM by genre

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const apiKey = '0b941991fb739be72fed42ae5e2a4891'
+const root = document.getElementById('root');
 
 fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
   .then(resp => resp.json())
@@ -6,7 +7,8 @@ fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
       .then(resp => resp.json())
       .then(genresResults => {
-        buildMovieGenreObject(moviesResults.results, genresResults.genres);
+        const movieGenres = buildMovieGenreObject(moviesResults.results, genresResults.genres);
+        insertIntoDOM(movieGenres);
       });
   });  
 
@@ -14,21 +16,56 @@ fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
 function buildMovieGenreObject(movies, genres) {
   movieGenres = {}
 
-  movies.forEach(movie => {
-    movie.genre_ids.forEach(genre_id => {
-      let movieGenre = genres.find(genre => genre.id === genre_id);
-      
-      if(movieGenres[movieGenre.name] === undefined) {
-        movieGenres[movieGenre.name] = [movie];
-      } else {
-        movieGenres[movieGenre.name].push(movie);
+  genres.forEach(genre => {
+    movies.forEach(function(movie) {
+      if (movie.genre_ids.includes(genre.id)) {
+        if(movieGenres[genre.name] === undefined) {
+          movieGenres[genre.name] = [movie];
+        } else {
+          movieGenres[genre.name].push(movie);
+        }
       }
-    });
+    })
   });
 
-  console.log(movieGenres);
+  return movieGenres;
 }
 
-function insertIntoDOM(crazyeMovieGenreObject) {
+function insertIntoDOM(movieGenres) {
+  let html = "";
 
+  for(const genre in movieGenres) {
+    html += `<div class="titleList">
+              <div class="title">
+                <h1>${genre}</h1>
+                <div class="titles-wrapper">`
+    
+    movieGenres[genre].forEach(movie => {
+      html += `
+      <div class="movie">
+        <img
+          src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}"
+        />
+        <div class="overlay">
+          <div class="title">${movie.title}</div>
+          <div class="rating">${movie.vote_average}/10</div>
+          <div class="plot">
+            ${movie.overview}
+          </div>
+          <div class="listToggle">
+            <div>
+              <i class="fa fa-fw fa-plus"></i>
+              <i class="fa fa-fw fa-check"></i>
+            </div>
+          </div>
+        </div>
+      </div>`
+    });
+          
+    html += `</div>
+        </div>
+      </div>`
+  }
+
+  root.insertAdjacentHTML('beforeend', html);
 }
